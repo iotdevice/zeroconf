2eb8664da35bb64ddc9869df67d88e656649b4a9
Yu Fang <yu@iotserv.com>
2021-01-30 23:38:37 +0800

    update

--- examples/resolv/client.go
+++ examples/resolv/client.go
@@ -10,7 +10,8 @@ import (
 )
 
 var (
-	service  = flag.String("service", "_workstation._tcp", "Set the service category to look for devices.")
+	//service  = flag.String("service", "_iotdevice._tcp", "Set the service category to look for devices.")
+	service  = flag.String("service", "_services._dns-sd._udp", "Set the service category to look for devices.")
 	domain   = flag.String("domain", "local", "Set the search domain. For local networks, default is fine.")
 	waitTime = flag.Int("wait", 10, "Duration in [s] to run discovery.")
 )
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/iotdevice/zeroconf
+
+go 1.15
+
+require (
+	github.com/cenkalti/backoff v2.2.1+incompatible
+	github.com/grandcat/zeroconf v1.0.0
+	github.com/miekg/dns v1.1.35
+	github.com/pkg/errors v0.9.1
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
+)
